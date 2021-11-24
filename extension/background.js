9ed3fde32c5b093d67f4f08feb9d5d4103085ef2
Fabio Sabbion <fabiosabbion.apps@gmail.com>
2021-11-24 20:01:42 +0100

    implemented basic logic of download routing

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,32 @@
 chrome.downloads.onDeterminingFilename.addListener(function (downloadItem, suggest) {
-    // console.log(downloadItem.filename);
-    // suggest({ filename: "analisi/" + downloadItem.filename });
+    let filename = downloadItem.filename;
+    // DEBUG:
+    // console.log(filename);
+
+    let url = downloadItem.referrer
+    // DEBUG:
+    console.log(url);
+
+    let ext = filename.split('.').pop();
+    // DEBUG:
+    // console.log(ext);
+
+    chrome.storage.local.get(url, function (result) {
+        if (result[url]) {
+            let finalPath = result[url] + '/' + filename;
+            // DEBUG:
+            console.log('Final path: ' + finalPath);
+
+            suggest({ filename: finalPath });
+        } else {
+            suggest({ filename: "Downloads/" + filename });
+        }
+    });
+    return true;
+
+    // TODO:
+    // - choose default download path
+    // - control download path format
+    // - add support for filetype to folder mapping
+    // - add support for rules priority
 });
