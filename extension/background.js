a828624e213ab0e16465f57f242c724e95e371d9
Fabio Sabbion <fabiosabbion.apps@gmail.com>
2021-11-25 13:52:28 +0100

    added support for filetype and url mappings

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,5 @@
+import * as utils from "./utils";
+
 chrome.downloads.onDeterminingFilename.addListener(function (downloadItem, suggest) {
     let filename = downloadItem.filename;
     // DEBUG:
@@ -5,32 +7,58 @@ chrome.downloads.onDeterminingFilename.addListener(function (downloadItem, sugge
 
     let url = downloadItem.referrer
     // DEBUG:
-    console.log(url);
+    // console.log(url);
 
-    let ext = filename.split('.').pop();
-    // DEBUG:
-    // console.log(ext);
+    function urlMapping(url, filename) {
+        // DEBUG:
+        console.log("Performing URL mapping: " + url);
+        chrome.storage.local.get([url, "defaultDownloadFolder"], function (result) {
+            if (result[url]) {
+                let finalPath = result[url] + '/' + filename;
+                // DEBUG:
+                console.log('Final path: ' + finalPath);
 
-    chrome.storage.local.get([url, "defaultDownloadFolder"], function (result) {
-        if (result[url]) {
-            let finalPath = result[url] + '/' + filename;
-            // DEBUG:
-            console.log('Final path: ' + finalPath);
+                suggest({ filename: finalPath });
+            } else {
+                // DEBUG:
+                console.log('No path for this url: using default folder');
 
-            suggest({ filename: finalPath });
-        } else {
-            // DEBUG:
-            console.log('No path for this url: using default folder');
-
-            if (result["defaultDownloadFolder"]) {
-                suggest({ filename: result["defaultDownloadFolder"] + "/" + filename });
+                if (result["defaultDownloadFolder"]) {
+                    suggest({ filename: result["defaultDownloadFolder"] + "/" + filename });
+                }
             }
-        }
-    });
+        });
+    }
+
+    function filetypeMapping(filename) {
+        // DEBUG:
+        console.log("Performing filetype mapping: " + filename);
+        let ext = filename.split('.').pop();
+        let filetype = utils.extToFiletype(ext);
+        chrome.storage.local.get([filetype, "defaultDownloadFolder"], function (result) {
+            if (result[filetype]) {
+                let finalPath = result[filetype] + '/' + filename;
+                // DEBUG:
+                console.log('Final path: ' + finalPath);
+
+                suggest({ filename: finalPath });
+            } else {
+                // DEBUG:
+                console.log('No path for this filetype: using default folder');
+
+                if (result["defaultDownloadFolder"]) {
+                    suggest({ filename: result["defaultDownloadFolder"] + "/" + filename });
+                }
+            }
+        });
+
+    }
+
+
     return true;
 
     // TODO:
-    // - control download path format
-    // - add support for filetype to folder mapping
     // - add support for rules priority
+    // - write get started page
+    // - import/export rules in JSON
 });
