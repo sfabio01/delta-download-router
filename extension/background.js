822feac43ed50c404b7dd4a695c47269ac6ca21b
Fabio Sabbion <fabiosabbion.apps@gmail.com>
2021-11-25 20:34:45 +0100

    implemented rules priority logic

--- extension/background.js
+++ extension/background.js
@@ -1,64 +1,169 @@
-import * as utils from "./utils";
-
 chrome.downloads.onDeterminingFilename.addListener(function (downloadItem, suggest) {
     let filename = downloadItem.filename;
     // DEBUG:
-    // console.log(filename);
+    console.log(filename);
 
-    let url = downloadItem.referrer
+    let url = downloadItem.referrer;
     // DEBUG:
-    // console.log(url);
+    console.log(url);
 
-    function urlMapping(url, filename) {
+    chrome.storage.local.get("priorityList", async function (result) {
+        let priorityList = defaultPriorityList;
+        let matched = false;
+
+        if (result["priorityList"])
+            priorityList = result["priorityList"];
+
+        for (let item of priorityList) {
+            if (matched)
+                break;
+            switch (item) {
+                case "urlToFolder": {
+                    matched = await urlMapping(url, filename);
+                    break;
+                }
+                case "filetypeToFolder": {
+                    matched = await filetypeMapping(filename);
+                    break;
+                }
+            }
+        }
+
+        if (!matched) {
+            let result = await chrome.storage.local.get(["defaultDownloadFolder"]);
+            if (result["defaultDownloadFolder"]) {
+                suggest({ filename: result["defaultDownloadFolder"] + "/" + filename });
+            }
+        }
+    });
+
+    async function urlMapping(url, filename) {
         // DEBUG:
         console.log("Performing URL mapping: " + url);
-        chrome.storage.local.get([url, "defaultDownloadFolder"], function (result) {
-            if (result[url]) {
-                let finalPath = result[url] + '/' + filename;
-                // DEBUG:
-                console.log('Final path: ' + finalPath);
 
-                suggest({ filename: finalPath });
-            } else {
-                // DEBUG:
-                console.log('No path for this url: using default folder');
+        let result = await chrome.storage.local.get([url]);
 
-                if (result["defaultDownloadFolder"]) {
-                    suggest({ filename: result["defaultDownloadFolder"] + "/" + filename });
-                }
-            }
-        });
+        if (result[url]) {
+            let finalPath = result[url] + '/' + filename;
+            // DEBUG:
+            console.log('Final path: ' + finalPath);
+
+            suggest({ filename: finalPath });
+
+            return true;
+        }
+
+        // DEBUG:
+        console.log('No path for this url');
+
+        return false;
     }
 
-    function filetypeMapping(filename) {
+    async function filetypeMapping(filename) {
         // DEBUG:
         console.log("Performing filetype mapping: " + filename);
+
         let ext = filename.split('.').pop();
-        let filetype = utils.extToFiletype(ext);
-        chrome.storage.local.get([filetype, "defaultDownloadFolder"], function (result) {
-            if (result[filetype]) {
-                let finalPath = result[filetype] + '/' + filename;
-                // DEBUG:
-                console.log('Final path: ' + finalPath);
+        let filetype = extToFiletype(ext);
 
-                suggest({ filename: finalPath });
-            } else {
-                // DEBUG:
-                console.log('No path for this filetype: using default folder');
+        let result = await chrome.storage.local.get([filetype]);
 
-                if (result["defaultDownloadFolder"]) {
-                    suggest({ filename: result["defaultDownloadFolder"] + "/" + filename });
-                }
-            }
-        });
+        if (result[filetype]) {
+            let finalPath = result[filetype] + '/' + filename;
+            // DEBUG:
+            console.log('Final path: ' + finalPath);
 
+            suggest({ filename: finalPath });
+
+            return true;
+        }
+
+        // DEBUG:
+        console.log('No path for this filetype');
+
+        return false;
     }
 
 
     return true;
 
     // TODO:
-    // - add support for rules priority
     // - write get started page
     // - import/export rules in JSON
 });
+
+/* --- CONSTANTS & UTILS--- */
+
+const fileTypes = {
+    documents: ["pdf", "doc", "docx", "xls", "xlsx", "ppt", "pptx"],
+    images: ["jpg", "jpeg", "png", "gif", "bmp", "svg"],
+    audio: ["mp3", "wav", "aiff", "ogg"],
+    video: ["mp4", "avi", "mov", "mpeg", "mpg"],
+    archives: [
+        "zip",
+        "rar",
+        "7z",
+        "gz",
+        "bz2",
+        "tar",
+        "tgz",
+        "xz",
+        "z",
+    ],
+    programms: [
+        "exe",
+        "msi",
+        "apk",
+        "dmg",
+        "pkg",
+        "deb",
+        "rpm",
+        "bat",
+        "com",
+        "jar",
+        "appx",
+    ],
+    code: [
+        "xml",
+        "json",
+        "html",
+        "htm",
+        "php",
+        "css",
+        "js",
+        "py",
+        "java",
+        "c",
+        "cpp",
+        "h",
+        "hpp",
+        "cs",
+        "go",
+        "ts",
+        "dart",
+        "sh",
+        "md",
+        "yml",
+        "yaml",
+        "ini",
+        "cfg",
+        "conf",
+        "log",
+        "sql",
+        "db",
+        "dbf",
+        "mdb",
+        "sqlite",
+    ],
+};
+
+function extToFiletype(ext) {
+    for (let type in fileTypes) {
+        if (fileTypes[type].includes(ext)) {
+            return type;
+        }
+    }
+    return "defaultDownloadFolder";
+}
+
+const defaultPriorityList = ["urlToFolder", "filetypeToFolder"];
